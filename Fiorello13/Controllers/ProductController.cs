2158a4f59028426fc1e8f72177b2c719b1dc456e
[umid shamdinli] <[umidash@code.edu.az]>
2022-03-15 20:40:17 +0400

    Added Partial task

---
+++ Fiorello13/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Fiorello13.Data;
+using Fiorello13.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello13.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            List<Product> products = await _context.Products.Where(p=> p.IsDeleted==false)
+                .Include(m => m.Category)
+                .Include(m => m.Images)
+                .OrderByDescending(m=> m.Id)
+                .Take(8)
+                .ToListAsync();
+            return View(products);
+        }
+        public async Task<IActionResult> LoadMore()
+        {
+
+            List<Product> products = await _context.Products.Where(p => p.IsDeleted == false)
+               
+                .OrderByDescending(m => m.Id)
+                .Skip(4)
+                .Take(4)
+                .ToListAsync();
+            return Json(products);
+        }
+    }
+}
